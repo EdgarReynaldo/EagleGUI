ab3d3e9d59dec6d51185a73ced3df82c6e1a2ed6
bugsquasher <camelot70@peoplepc.com>
2016-10-24 19:28:28 -0500

    Initial commit for transforms. Update to EagleNotes.txt.

---
+++ include/Eagle/Transforms.hpp
@@ -0,0 +1,45 @@
+
+/*
+ *
+ *     _______       ___       ____      __       _______
+ *    /\  ____\    /|   \     /  __\    /\ \     /\  ____\
+ *    \ \ \___/_   ||  _ \   |  /__/____\ \ \    \ \ \___/_
+ *     \ \  ____\  || |_\ \  |\ \ /\_  _\\ \ \    \ \  ____\
+ *      \ \ \___/_ ||  ___ \ \ \ \\//\ \/ \ \ \____\ \ \___/_
+ *       \ \______\||_|__/\_\ \ \ \_\/ |   \ \_____\\ \______\
+ *        \/______/|/_/  \/_/  \_\_____/    \/_____/ \/______/
+ *
+ *
+ *    EAGLE
+ *    Edgar's Agile Gui Library and Extensions
+ *
+ *    Copyright 2009-2016+ by Edgar Reynaldo
+ *
+ *    See EagleLicense.txt for allowed uses of this library.
+ *
+ */
+
+#ifndef Transforms_HPP
+#define Transforms_HPP
+
+
+
+
+class Transform {
+   
+   
+   
+};
+
+
+
+
+
+
+
+#endif // Transforms_HPP
+
+
+
+
+
