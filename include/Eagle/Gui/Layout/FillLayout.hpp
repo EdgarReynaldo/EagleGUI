636c6525e07048fb40e9d5a74634b141413a5fb1
bugsquasher <camelot70@peoplepc.com>
2016-12-06 17:06:37 -0600

    Added FillLayout for Decorator class.

--- include/Eagle/Gui/Layout/FillLayout.hpp
+++ include/Eagle/Gui/Layout/FillLayout.hpp
@@ -7,13 +7,28 @@
 
 
 
+#include "Eagle/Gui/Layout/Layout.hpp"
+
 
 
 class FillLayout : public Layout {
    
+   virtual void ReserveSlots(int nslots);
+
+public :
+   
+   FillLayout();
    
    
+   virtual Rectangle RequestWidgetArea(int widget_slot , int newx , int newy , int newwidth , int newheight);
    
+   virtual void PlaceWidget(WidgetBase* widget , int slot);/// Only valid slot is zero
+   virtual void AddWidget(WidgetBase* widget);/// Will only add a widget to an empty layout
+
+
+
+   virtual std::ostream& DescribeTo(std::ostream& os , Indenter indent = Indenter()) const;
+
 };
 
 
---
+++ src/Gui/Layout/FillLayout.cpp
@@ -0,0 +1,66 @@
+
+
+
+
+#include "Eagle/Gui/Layout/FillLayout.hpp"
+
+#include "Eagle/StringWork.hpp"
+#include "Eagle/Exception.hpp"
+
+
+
+void FillLayout::ReserveSlots(int nslots) {
+   (void)nslots;
+   Layout::ReserveSlots(1);
+}
+
+
+
+FillLayout::FillLayout() :
+      Layout(StringPrintF("FillLayout at %p" , this))
+{}
+
+
+
+Rectangle FillLayout::RequestWidgetArea(int widget_slot , int newx , int newy , int newwidth , int newheight) {
+   (void)widget_slot;
+   (void)newx;
+   (void)newy;
+   (void)newwidth;
+   (void)newheight;
+   return InnerArea();
+}
+
+
+
+void FillLayout::PlaceWidget(WidgetBase* widget , int slot) {
+   if (slot != 0) {
+      throw EagleException(StringPrintF("FillLayout::PlaceWidget : slot #%d is not valid. Only slot #0 is valid.\n" , slot));
+   }
+   else {
+      Layout::PlaceWidget(widget , 0);
+   }
+}
+
+
+
+void FillLayout::AddWidget(WidgetBase* widget) {
+   if (NextFreeSlot() == -1) {
+      throw EagleException("FillLayout::AddWidget : FillLayout already full!\n");
+   }
+   else {
+      Layout::PlaceWidget(widget , 0);
+   }
+}
+
+
+
+std::ostream& FillLayout::DescribeTo(std::ostream& os , Indenter indent) const {
+   return Layout::DescribeTo(os , indent);
+}
+
+
+
+
+
+
