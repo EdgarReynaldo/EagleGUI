40da7325c34b4b720a58d95a8a472cc635c8c8df
Edgar Reynaldo <edgarreynaldo@members.allegro.cc>
2018-04-08 12:57:03 -0500

    ?

--- src/Position.cpp
+++ src/Position.cpp
@@ -3,8 +3,12 @@
 
 #include "Eagle/Position.hpp"
 
+Pos2F VectorF(const Pos2F& p , float r , float theta) {
+   return Pos2F(p.X() + r*cos(theta) , p.Y() + r*sin(theta));
+}
+
+
+
 Pos2D Vector(const Pos2D& p , double r , double theta) {
-   Pos2D p2 = p;
-   p2.MoveBy(r*cos(theta) , r*sin(theta));
-   return p2;
+   return Pos2D(p.X() + r*cos(theta) , p.Y() + r*sin(theta));
 }
