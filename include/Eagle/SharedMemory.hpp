a0b193b78f4ba37d3a257ce96bdc70471c3ac717
Edgar Reynaldo <edgarreynaldo@members.allegro.cc>
2018-07-28 18:19:42 -0500

    Show code

--- include/Eagle/SharedMemory.hpp
+++ include/Eagle/SharedMemory.hpp
@@ -27,22 +27,71 @@ public :
    }
 };
 
-template <class T>
-class SHAREDOBJECT : public std::shared_ptr<T> {
+template <typename T>
+class SHAREDOBJECT;
+
+template <typename T>
+SHAREDOBJECT<T> StackObject(T* t);
+
+template <typename T>
+SHAREDOBJECT<T> StackObject(T& t);
+
+template <typename T>
+SHAREDOBJECT<T> HeapObject(T* t);
+
+template <typename T>
+class SHAREDOBJECT {
+   std::shared_ptr<T> sptr;
+   SHAREDOBJECT(T* t, bool delete_me) :
+         sptr(t , DELETER<T>(delete_me))
+   {}
+   friend SHAREDOBJECT StackObject<T>(T* t);
+   friend SHAREDOBJECT StackObject<T>(T& t);
+   friend SHAREDOBJECT HeapObject<T>(T* t);
+
+public :
+   
+   SHAREDOBJECT() :
+         sptr(0)
+   {}
+
+   void reset() {sptr.reset();}
+   T* get() {return sptr.get();}
+   const std::shared_ptr<T>& ptr() {return sptr;}
+   const T* get() const {return sptr.get();}
+   
+   operator T* () {return sptr.get();}
+   operator const T* () const {return sptr.get();}
+   
+   T* operator->() {return sptr.get();}
+   const T* operator->() const {return sptr.get();}
+   
+   T& operator*() {return *sptr.get();}
+   const T& operator*() const {return *sptr.get();}
+
+   operator bool() const {return (bool)sptr.get();}
 };
 
-template <class T>
-SHAREDOBJECT<T> StackObject(T* t);
-template <class T>
-SHAREDOBJECT<T> StackObject(T& t);
+
+
+
+
+
+
+
+
 
 template <class T>
 SHAREDOBJECT<T> StackObject(T* t) {
-   return SHAREDOBJECT<T>(t,DELETER<T>(false));
+   return SHAREDOBJECT<T>(t,false);
 }
 template <class T>
 SHAREDOBJECT<T> StackObject(T& t) {
-   return SHAREDOBJECT<T>(&t,DELETER<T>(false));
+   return SHAREDOBJECT<T>(&t,false);
+}
+template <class T>
+SHAREDOBJECT<T> HeapObject(T* t) {
+   return SHAREDOBJECT<T>(t,true);
 }
 
 
