9c13d94311ed014c84f110b9cf2a5fbb034a97b9
Edgar Reynaldo <edgarreynaldo@members.allegro.cc>
2020-03-08 13:53:00 -0500

    Moved EditText

---
+++ include/Eagle/Gui/Text/EditText.hpp
@@ -0,0 +1,57 @@
+
+
+
+#ifndef EditText_HPP
+#define EditText_HPP
+
+
+
+#include "Eagle/Gui/Text/SelectText.hpp"
+
+
+
+class EditText : public SelectText {
+   
+protected :
+   
+   bool insert_mode;
+   unsigned int pixscroll;
+   unsigned int NTABSPACES;
+   
+   
+   
+   /// WIDGETBASE
+   virtual int PrivateHandleEvent(EagleEvent e);
+   virtual int PrivateCheckInputs();
+   virtual void PrivateDisplay(EagleGraphicsContext* win , int xpos , int ypos);
+   virtual int PrivateUpdate(double tsec);
+
+   virtual void OnAreaChanged();
+   virtual void OnFlagChanged(WIDGET_FLAGS f , bool on);
+   
+   /// SelectText
+   virtual void DrawCaret(EagleGraphicsContext* win , int xpos , int ypos);
+
+   /// EditText
+   void DrawOverwriteCaret(EagleGraphicsContext* win , int xpos , int ypos);
+   
+   void Insert(std::string instr , int caretLine , int caretPos);
+
+   void DeleteSelection(int selLineStart , int selLineStop , int selLeft , int selRight);
+   void Overwrite(std::string instr , int selLineStart , int selLineStop , int selLeft , int selRight);
+   
+public :
+   
+   EditText(std::string classname = "EditText" , std::string objname = "Nemo");
+   
+   bool Insert();
+   bool Overwrite();
+   
+   void SetTabSpacing(unsigned int ntabspaces);
+};
+
+
+
+
+#endif // EditText_HPP
+
