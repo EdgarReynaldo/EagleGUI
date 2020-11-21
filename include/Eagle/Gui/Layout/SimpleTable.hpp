cb0bba256fdc2f6b87a2f06cebe2e1c70a46dd3a
Edgar Reynaldo <edgarreynaldo@members.allegro.cc>
2020-11-21 01:09:21 -0600

    Update copyright 2

--- include/Eagle/Gui/Layout/SimpleTable.hpp
+++ include/Eagle/Gui/Layout/SimpleTable.hpp
@@ -1,17 +1,34 @@
 
-
-
+/**
+ *
+ *         _______       ___       ____      __       _______
+ *        /\  ____\    /|   \     /  __\    /\ \     /\  ____\
+ *        \ \ \___/_   ||  _ \   |  /__/____\ \ \    \ \ \___/_
+ *         \ \  ____\  || |_\ \  |\ \ /\_  _\\ \ \    \ \  ____\
+ *          \ \ \___/_ ||  ___ \ \ \ \\//\ \/ \ \ \____\ \ \___/_
+ *           \ \______\||_|__/\_\ \ \ \_\/ |   \ \_____\\ \______\
+ *            \/______/|/_/  \/_/  \_\_____/    \/_____/ \/______/
+ *
+ *
+ *    Eagle Agile Gui Library and Extensions
+ *
+ *    Copyright 2009-2021+ by Edgar Reynaldo
+ *
+ *    See EagleLicense.txt for allowed uses of this library.
+ *
+ * @file 
+ * @brief
+ * 
+ * 
+ * 
+ */
 
 #ifndef SimpleTableLayout_HPP
 #define SimpleTableLayout_HPP
 
 
+
 #include "Eagle/Gui/Layout/Layout.hpp"
-
-
-
-
-
 
 
 
