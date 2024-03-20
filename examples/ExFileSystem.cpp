282f7a9baccb68b2820e986e5012fb2962034f6d
Zorro <edgarreynaldo@members.allegro.cc>
2024-03-20 14:12:47 -0500

    Misc update

--- examples/ExFileSystem.cpp
+++ examples/ExFileSystem.cpp
@@ -6,7 +6,7 @@
 #include "allegro5/allegro.h"
 #include "allegro5/allegro_native_dialog.h"
 
-
+#include "physfs.h"
 
 #include "Eagle/Gui/Menu/SimpleMenu.hpp"
 
@@ -19,25 +19,27 @@ void PipeToEagleLog(const char* str) {
 
 
 int main(int argc , char** argv) {
-   
+
    (void)argc;
    (void)argv;
-   
+
+   PHYSFS_init(argv[0]);
+
    al_set_config_value(al_get_system_config() , "trace" , "level" , "error");
-   
+
    al_register_trace_handler(PipeToEagleLog);
-   
+
    SendOutputToFile("ExMenu.log" , "" , false);
-   
+
    Allegro5System* sys = GetAllegro5System();
-   
+
    if (sys->Initialize(EAGLE_FULL_SETUP) != EAGLE_FULL_SETUP) {
       EagleWarn() << "Some subsystems not initialized. Proceeding" << std::endl;
    }
-   
+
    int sw = 800;
    int sh = 600;
-   
+
    bool dialogs = al_init_native_dialog_addon();
    if (!al_is_native_dialog_addon_initialized()) {
       EagleCritical() << "Failed to initialize the dialog addon." << std::endl;
@@ -45,27 +47,27 @@ int main(int argc , char** argv) {
    if (dialogs) {
       EagleInfo() << "Allegro native dialog addon initialized" << std::endl;
    }
-   
-   
-   
+
+
+
    EagleGraphicsContext* win = sys->CreateGraphicsContext("Main Window" , sw , sh , EAGLE_OPENGL | EAGLE_WINDOWED | EAGLE_RESIZABLE);
-   
+
    EAGLE_ASSERT(win && win->Valid());
-   
+
    EagleFont* font = win->GetFont("Data/Fonts/Verdana.ttf" , -20);
-   
+
    EAGLE_ASSERT(font && font->Valid());
-   
+
    EagleImage* checkbox_down = win->LoadImageFromFile("Data/Images/Checkbox_Checked.png");
    EagleImage* checkbox_up = win->LoadImageFromFile("Data/Images/Checkbox_Unchecked.png");
-   
-   
+
+
    WidgetHandler gui(win , "GUI" , "Example GUI");
-   
+
    gui.SetupBuffer(sw , sh , win);
-   
+
    gui.SetWidgetArea(Rectangle(0 , 0 , sw , sh));
-   
+
 
    SIMPLE_MENU_ITEM fmenu[] = {{SPRING_BTN , false , (EagleImage*)0 , (EagleImage*)0 , font , "&Load"  , input_key_press(EAGLE_KEY_L) , "L"},
                                {SPRING_BTN , false , (EagleImage*)0 , (EagleImage*)0 , font , "&Save"  , input_key_press(EAGLE_KEY_S) , "S"},
@@ -78,34 +80,34 @@ int main(int argc , char** argv) {
 
    SIMPLE_MENU_BAR_ITEM mbar[] = {{font , "&File"    , input_key_press(EAGLE_KEY_F)},
                                   {font , "&Options" , input_key_press(EAGLE_KEY_O)}};
-   
-   
+
+
    ClassicMenu options;
    ClassicMenu foptions;
    ClassicMenuBar menubar;
-   
+
    options.SetItems(&omenu[0] , 3);
    foptions.SetItems(&fmenu[0] , 4);
    menubar.SetBarItems(&mbar[0] , 2);
-   
+
    menubar.SetSubMenu(0 , &foptions);
    menubar.SetSubMenu(1 , &options);
-   
+
    RelativeLayout rlayout;
    rlayout.Resize(3);
-   
+
    rlayout.PlaceWidget(&menubar  , 0 , LayoutRectangle(0 , 0 , 1 , 0.1));
    rlayout.PlaceWidget(&foptions , 1 , LayoutRectangle(0.25 , 0.25 , 0.25 , 0.25));
    rlayout.PlaceWidget(&options  , 2 , LayoutRectangle(0.5 , 0.25 , 0.25 , 0.25));
-   
+
    gui.SetRootLayout(&rlayout);
-   
+
 //   EagleInfo() << gui << std::endl << std::endl;
-   
+
    sys->GetSystemTimer()->Start();
-   
+
    bool show = true;
-   
+
    bool quit = false;
    bool redraw = true;
    while (!quit) {
@@ -118,12 +120,12 @@ int main(int argc , char** argv) {
       }
       while (!sys->UpToDate()) {
          EagleEvent e = sys->WaitForSystemEventAndUpdateState();
-         
+
          if (e.type == EAGLE_EVENT_DISPLAY_RESIZE) {
             gui.SetWidgetArea(Rectangle(0 , 0 , e.window->Width() , e.window->Height()) , false);
             win->AcknowledgeResize();
          }
-         
+
          if (e.type == EAGLE_EVENT_KEY_DOWN && e.keyboard.keycode == EAGLE_KEY_ESCAPE) {
             quit = true;
          }
