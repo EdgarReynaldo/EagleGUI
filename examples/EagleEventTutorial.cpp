2a60b139f76d772ff644fb1e79ba3b4055e1c877
Marco Polo <marc@medavenport.com>
2024-04-05 13:09:06 -0500

    Fixing event spam

--- examples/EagleEventTutorial.cpp
+++ examples/EagleEventTutorial.cpp
@@ -6,51 +6,52 @@
 
 
 int main(int argc , char** argv) {
-   
+
    (void)argc;
    (void)argv;
-   
+
    /// To do most anything in Eagle, we need a system driver
    Allegro5System* sys = GetAllegro5System();
    EAGLE_ASSERT(sys);/// EAGLE_ASSERT is a regular assert but it logs and
                      /// throws an exception if null. EAGLE_DEBUG must
                      /// be defined or it will reduce to nothing.
-   
+
    /// Now we have to initialize the system. Generally EAGLE_FULL_SETUP is used
    int sysret = sys->Initialize(EAGLE_FULL_SETUP);/// Return value is the
    EAGLE_ASSERT(sysret & EAGLE_STANDARD_SETUP);
-   
+
    EagleGraphicsContext* win = sys->CreateGraphicsContext("Our Window" , 640 , 480 , EAGLE_OPENGL | EAGLE_WINDOWED);
    EAGLE_ASSERT(win && win->Valid());
-   
+
    win->Clear();
    win->FlipDisplay();
-   
+
    EagleEventHandler* q = sys->GetSystemQueue();
-   
+
    bool quit = false;
    bool redraw = true;
    bool pause = false;
    std::string message;
-   
+
    std::deque<std::string> messages;
-   
+
    sys->GetSystemTimer()->Start();
-   
+
+   int mscnt = 0;
+
    while (!quit || pause) {
       pause = false;
       if (redraw) {
+         win->Clear();
          if (messages.size()) {
-            std::cout << messages.back() << std::endl;
-            win->Clear();
             int i = 0;
             for (std::deque<std::string>::iterator it = messages.begin() ; it != messages.end() ; ++it) {
                win->DrawTextString(win->DefaultFont() , *it , 320 , 460 - 10*i , EagleColor(0,255,0) , HALIGN_CENTER , VALIGN_CENTER);
                ++i;
             }
-            win->FlipDisplay();
-            redraw = false;
          }
+         win->FlipDisplay();
+         redraw = false;
          while (messages.size() > 9) {
             messages.pop_front();
          }
@@ -58,6 +59,12 @@ int main(int argc , char** argv) {
       if (q->HasEvent()) {
          redraw = true;
          EagleEvent e = sys->WaitForSystemEventAndUpdateState();
+         if (e.type == EAGLE_EVENT_MOUSE_AXES) {
+            mscnt++;
+         }
+         else {
+            mscnt = 0;
+         }
          if (e.type == EAGLE_EVENT_DISPLAY_CLOSE) {
             message = "Display closed.";
             messages.push_back(message);
@@ -72,10 +79,15 @@ int main(int argc , char** argv) {
          }
          else if (e.type == EAGLE_EVENT_TIMER) {
             redraw = true;
-            (void)0;
+         }
+         else if (e.type == EAGLE_EVENT_MOUSE_AXES) {
+            if (messages.size() &&
+                messages.back().compare(0 , 22 , "EAGLE_EVENT_MOUSE_AXES") == 0) {
+               messages[messages.size() - 1] = StringPrintF("EAGLE_EVENT_MOUSE_AXES %d" , mscnt);
+            }
          }
          else {
-            message = std::string("Message ") + EagleEventName(e.type) + " received.";
+            message = EagleEventName(e.type);
             messages.push_back(message);
             pause = true;
          }
@@ -83,3 +95,6 @@ int main(int argc , char** argv) {
    }
    return 0;
 }
+
+
+
