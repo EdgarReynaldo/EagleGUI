2ce899fdd18169cb0a9662e8043f498e85e288d8
Zorro <edgarreynaldo@members.allegro.cc>
2024-08-17 10:12:46 -0500

    Updated main header to include most headers

--- include/Eagle.hpp
+++ include/Eagle.hpp
@@ -12,7 +12,7 @@
  *
  *    Eagle Agile Gui Library and Extensions
  *
- *    Copyright 2009-2021+ by Edgar Reynaldo
+ *    Copyright 2009-2024+ by Edgar Reynaldo
  *
  *    See EagleLicense.txt for allowed uses of this library.
  *
@@ -32,7 +32,9 @@
 
 #include "Eagle/AnimationBase.hpp"
 #include "Eagle/Area.hpp"
+#include "Eagle/BinFileStream.hpp"
 #include "Eagle/BinStream.hpp"
+#include "Eagle/BoxArea.hpp"
 #include "Eagle/Clipboard.hpp"
 #include "Eagle/Color.hpp"
 #include "Eagle/ConfigFile.hpp"
@@ -45,35 +47,50 @@
 #include "Eagle/FileWork.hpp"
 #include "Eagle/FileSystem.hpp"
 #include "Eagle/Font.hpp"
+#include "Eagle/FontManager.hpp"
 #include "Eagle/GraphicsContext.hpp"
+#include "Eagle/GraphicsHardware.hpp"
 #include "Eagle/Gui.hpp"
 #include "Eagle/Image.hpp"
 #include "Eagle/ImageAtlas.hpp"
 #include "Eagle/Indenter.hpp"
 #include "Eagle/InputHandler.hpp"
+#include "Eagle/Input.hpp"
+#include "Eagle/InputGroup.hpp"
+#include "Eagle/InputAssignment.hpp"
+#include "Eagle/Interpolate.hpp"
 #include "Eagle/Lib.hpp"
+#include "Eagle/LineEquations.hpp"
 #include "Eagle/Logging.hpp"
 #include "Eagle/Math.hpp"
 #include "Eagle/MemberFunctors.hpp"
+#include "Eagle/MemFile.hpp"
+#include "Eagle/Mesh.hpp"
 #include "Eagle/MousePointer.hpp"
 #include "Eagle/Mutexes.hpp"
 #include "Eagle/NinePatch.hpp"
 #include "Eagle/ObjectRegistry.hpp"
 #include "Eagle/ObjectInfo.hpp"
 #include "Eagle/Object.hpp"
+#include "Eagle/ObjModel.hpp"
 #include "Eagle/Position.hpp"
 #include "Eagle/Random.hpp"
 #include "Eagle/ResourceLib.hpp"
 #include "Eagle/Resources.hpp"
 #include "Eagle/Sound.hpp"
 #include "Eagle/SoundManager.hpp"
+#include "Eagle/SpaceCamera.hpp"
 #include "Eagle/StringWork.hpp"
 #include "Eagle/System.hpp"
+#include "Eagle/TextFile.hpp"
 #include "Eagle/Threads.hpp"
 #include "Eagle/Time.hpp"
 #include "Eagle/Timer.hpp"
 #include "Eagle/Transforms.hpp"
 #include "Eagle/Transitions.hpp"
+#include "Eagle/Vec2.hpp"
+#include "Eagle/Vec3.hpp"
+#include "Eagle/Vertex.hpp"
 #include "Eagle/WindowManager.hpp"
 
 
