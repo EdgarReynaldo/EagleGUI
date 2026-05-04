f505868f50c440548486e5bc3d7cae76752b4f74
Zorro <edgarreynaldo@members.allegro.cc>
2026-05-04 17:34:19 -0500

    closer to working

--- examples/UsingWidgetsTutorial.cpp
+++ examples/UsingWidgetsTutorial.cpp
@@ -39,6 +39,9 @@ public:
 
 
 
+void SetSliders(WidgetColorset* wc , RGBASlider* slarray4 , EagleColor c);
+
+
 
 int main(int argc , char** argv) {
    
@@ -65,8 +68,11 @@ int main(int argc , char** argv) {
    
    EagleLog() << "Setup eagle successfully" << std::endl;
    
+   int sw = 1024;
+   int sh = 768;
+   
    /// Create a named window size 800x600 using an OpenGL window centered on the display
-   EagleGraphicsContext* win = sys->CreateGraphicsContext("Main window" , 800 , 600 , EAGLE_OPENGL | EAGLE_WINDOWED , -1 , -1);
+   EagleGraphicsContext* win = sys->CreateGraphicsContext("Main window" , sw , sh , EAGLE_OPENGL | EAGLE_WINDOWED , -1 , -1);
    
    /// Use our EAGLE_ASSERT mechanism to check the validity of the window
    EAGLE_ASSERT(win && win->Valid());
@@ -84,62 +90,80 @@ int main(int argc , char** argv) {
    
    /// To handle all our widgets, we need a WidgetHandler
    WidgetHandler gui(win);
-   gui.SetupBuffer(800,600,win);
+   gui.SetupBuffer(sw , sh , win);
    
    /// Since a WidgetHandler is derived from WidgetBase, we can use all our widget functions on our gui
    /// Widgets start at a size of 0,0 and position of 0,0 so we need to give them an area to occupy. Layouts do this for us
    /// But here we see how to do it manually
-   gui.SetWidgetArea(Rectangle(100,100,600,400));/// Set our position to 200,200 and our size to 400x300
+   gui.SetWidgetArea(Rectangle(0 , 0 , sw , sh));/// Set our position to 200,200 and our size to 400x300
 
    WidgetColorset wc = *gui.GetWidgetColorset();/// Get a copy of our gui's colors
    wc[SDCOL] = GetColorByName("purple");/// Set the shadow color to purple   wc[BGCOL] = GetColorByName("blue");/// Set the background color to blue
    wc[MGCOL] = GetColorByName("dark_gray");/// Set the middle ground color to dark gray
    wc[FGCOL] = GetColorByName("light_gray");/// Set the foreground color to light gray
    wc[HLCOL] = GetColorByName("green");/// Set the highlight color to green
+   wc[HVRCOL] = GetColorByName("green");/// Set the highlight color to green
    wc[TXTCOL] = GetColorByName("white");/// Set the text color to white
    
    /// Now that we've finished setting our colors we assign them back to the gui widget
+   WidgetColorset wc2 = wc;
    gui.SetWidgetColorset(wc);
    
    /// A gui comes with a dumb root layout. If you want to position everything manually, this is the way.
    /// However, setting the root layout will allow you to position all widgets added to the gui
-   /// Here we're going to use a flow layout for the root layout
+   /// Here we're going to use a flow layout for the root layout, which holds several other widget layouts
    FlowLayout flow;
    gui.SetRootLayout(&flow);
    
    
-   flow.SetFlowDirection(FLOW_FAVOR_VERTICAL);/// can be FLOW_FAVORED_VERTICAL or FLOW_FAVORED_HORIZONTAL
+   flow.SetFlowDirection(FLOW_FAVOR_HORIZONTAL);/// can be FLOW_FAVOR_VERTICAL or FLOW_FAVOR_HORIZONTAL
    flow.SetAnchorPosition(FBOX_ANCHOR_NW);/// Can be FBOX_ANCHOR_* [NW SE SW NE]
    flow.SetAlignment(HALIGN_CENTER , VALIGN_CENTER);/// Left top is default alignment, let's use centered alignment
    flow.SetBoxSpacing(BOX_SPACE_EVEN);/// Spread out the extra space evenly
    
-   flow.Resize(16);
+//   flow.Resize(16);
 
+   GridLayout cgrid(1 , EAGLE_NUMCOLORS , "GridLayout" , "Color button grid");
+   cgrid.SetPreferredSize(125,220);
+   
+   GridLayout rgbagrid(4 , 1 , "GridLayout" , "RGBA grid");
+   rgbagrid.SetPreferredSize(200 , 256);
+   
+   
+   flow.Resize(2);
+   flow.AddWidget(&cgrid);
+   flow.AddWidget(&rgbagrid);
+   
    GuiButton clrbtns[EAGLE_NUMCOLORS];/// color buttons
    RadioGroup clrgroup;/// make them a radio button
    for (unsigned int n = 0 ; n < EAGLE_NUMCOLORS ; ++n) {
-      clrbtns[n].SetButtonType(RECTANGLE_BTN , SPRING_BTN , BUTTON_CLASS_HOVER);
+      clrbtns[n].SetButtonType(RECTANGLE_BTN , TOGGLE_BTN , BUTTON_CLASS_HOVER);
       clrbtns[n].SetLabel(WidgetColorName((WIDGETCOLOR)n));
-      clrbtns[n].SetPreferredSize(200 , 20);
+      clrbtns[n].SetPreferredSize(125 , 20);
       clrbtns[n].SetFont(gfont);
-      flow.AddWidget(&clrbtns[n]);
+      cgrid.PlaceWidget(&clrbtns[n] , n);
       clrgroup.AddRadioButton(&clrbtns[n]);
    }
-   clrgroup.SelectButton(&clrbtns[0]);
+   int selected_color = (int)SDCOL;
+   clrgroup.SelectButton(&clrbtns[selected_color]);
+   
+   wc2 = wc;
    
    RGBASlider rgbaslider[4];/// RGBA sliders
    for (unsigned int n = 0 ; n < 4 ; ++n) {
+      rgbaslider[n].SetWidgetColorset(wc2);
       rgbaslider[n].type = (CSLIDER)n;
       rgbaslider[n].SetPreferredSize(50 , 256);
-      flow.AddWidget(&rgbaslider[n]);
+      rgbagrid.PlaceWidget(&rgbaslider[n] , n);
    }
    
+   EagleColor c = wc2[selected_color];
+   
+   SetSliders(&wc2 , rgbaslider , c);
    
    
    
-   
-   
-   
+   gui.SetFullRedraw();
    
    bool redraw = true;
    bool quit = false;
@@ -155,10 +179,28 @@ int main(int argc , char** argv) {
       
       do {
          EagleEvent e = sys->WaitForSystemEventAndUpdateState();
+         gui.HandleEvent(e);
+         WidgetMsg msg;
+         while (gui.HasMessages()) {
+            msg = gui.TakeNextMessage();
+            if (msg.Topic() == TOPIC_BUTTON_WIDGET) {
+               /// color button pressed, reset rgba sliders to wc[selected_color]
+               SetSliders(&wc2 , rgbaslider , wc[selected_color]);
+
+            }
+            else if (msg.Topic() == TOPIC_SLIDER) {
+               /// A color slider was changed, reset the BGCOL of the selected color button
+               SHAREDOBJECT<WidgetColorset> wcshared = clrbtns[selected_color].GetWidgetColorset();
+               (*wcshared.get())[BGCOL] = EagleColor(rgbaslider[0].GetColor(BGCOL).R() , rgbaslider[1].GetColor(BGCOL).G() ,
+                                                  rgbaslider[2].GetColor(BGCOL).B() , rgbaslider[3].GetColor(BGCOL).A());
+               clrbtns[selected_color].SetWidgetColorset(wcshared);
+               SetSliders(wcshared.get() , rgbaslider , (*wcshared.get())[BGCOL]);
+            }
+         }
          if (e.type == EAGLE_EVENT_TIMER) {
             redraw = true;
-         }
-         else {
+            gui.Update(e.timer.eagle_timer_source->SPT());
+         } else {
             if (e.type == EAGLE_EVENT_KEY_DOWN && e.keyboard.keycode == EAGLE_KEY_ESCAPE) {
                quit = true;
             }
@@ -185,14 +227,29 @@ int main(int argc , char** argv) {
 
 void RGBASlider::PrivateDisplay(EagleGraphicsContext* win , int xpos , int ypos) {
    Slider::PrivateDisplay(win , xpos , ypos);
-   
+   xpos += InnerArea().CX();
+   ypos += InnerArea().CY();
    char c[4] = {'R' , 'G' , 'B' , 'A'};
    win->DrawTextString(win->GetFont("Data/Fonts/Verdana.ttf" , -16) , StringPrintF("%c" , c[type]) ,
-                       xpos + OuterArea().CX() , ypos + OuterArea().CY() , GetColor(TXTCOL) , HALIGN_CENTER , VALIGN_CENTER);
+                       xpos , ypos , GetColor(TXTCOL) , HALIGN_CENTER , VALIGN_CENTER);
 }
 
 
 
+void SetSliders(WidgetColorset* wc , RGBASlider* slarray4 , EagleColor c) {
+   WidgetColorset wc2;
+   if (wc) {
+      wc2 = *wc;
+   }
+   wc2[MGCOL] = EagleColor(c.R() , 0 , 0 , 255);
+   slarray4[0].SetWidgetColorset(wc2);
+   wc2[MGCOL] = EagleColor(0 , c.G() , 0 , 255);
+   slarray4[1].SetWidgetColorset(wc2);
+   wc2[MGCOL] = EagleColor(0 , 0 , c.B() , 255);
+   slarray4[2].SetWidgetColorset(wc2);
+   wc2[MGCOL] = EagleColor(c.A() , c.A() , c.A() , c.A());
+   slarray4[3].SetWidgetColorset(wc2);
+}
 
 
 
