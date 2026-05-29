bd4ace6155b0ec4ba86eea88ea3d78e1d02bdcce
Zorro <edgarreynaldo@members.allegro.cc>
2026-05-29 11:00:24 -0500

    First commit, basic empty loop

--- src/TINS26/tins.cpp
+++ src/TINS26/tins.cpp
@@ -1,23 +1,81 @@
 
+/**
+
+TINS 2026 @Copyright Marc Davenport aka bugsquasher aka Modulo5k aka Edgar Reynaldo
+
+Here is a brief summary: 
+1. It's a zoo! 
+2. Science fiction reference. 
+3. Catastrophic overflow. 
+4. Quick save. 
+5. Bonus: Let a chatbot replace a rule.
 
 
+
+*/
+#include "Eagle.hpp"
+#include "Eagle/backends/Allegro5Backend.hpp"
+#include "Physfs.h"
 
 int main(int argc , char** argv) {
 
    (void)argc;
    (void)argv;
 
+   PHYSFS_init(argv[0]);
+   
+   /// To do most anything in Eagle, we need a system driver
+   Allegro5System* sys = GetAllegro5System();
+   EAGLE_ASSERT(sys);/// EAGLE_ASSERT is a regular assert but it logs and
+                     /// throws an exception if null. EAGLE_DEBUG must
+                     /// be defined or it will reduce to nothing.
+   
+   /// Now we have to initialize the system. Generally EAGLE_FULL_SETUP is used
+   int sysret = sys->Initialize(EAGLE_FULL_SETUP);/// Return value is the
+                                                  /// degree of success
+   if (EAGLE_FULL_SETUP != sysret) {
+      /// Some part of setup failed
+      PrintFailedEagleInitStates(EAGLE_FULL_SETUP , sysret);/// Log the failure
+      /// Test for ...
+      if (!(sysret & EAGLE_STANDARD_SETUP)) {
+         /// Failed to init an essential service
+         return -1;
+      }
+      /// Just keep going. Either touch or shaders failed but we don't need
+      /// them in this case
+   }
+   
+   EagleLog() << "Setup eagle successfully" << std::endl;
 
 
-
-
-
-
-
-
-
-
-
+   EagleGraphicsContext* win = sys->CreateGraphicsContext("TINS2026" , 1024 , 768 , EAGLE_WINDOWED | EAGLE_OPENGL);
+   EAGLE_ASSERT(win && win->Valid());
+   
+   bool quit = false;
+   bool redraw = true;
+   sys->GetSystemTimer()->Start();/// The system timer is created automatically, but needs to be manually started
+   
+   while (!quit) {
+      if (redraw) {
+         win->Clear();
+         win->FlipDisplay();
+      }
+      do {
+         /// Let's handle two birds with one stone. WaitForEventAndUpdateState will do this for us.
+         /// WaitForEvent will wait for an event from the system queue. Update state will update the input handler for us.
+         /// We rarely want to do this manually.
+         EagleEvent ee = sys->WaitForSystemEventAndUpdateState();
+         if (ee.type == EAGLE_EVENT_TIMER) {
+            redraw = true;
+         }
+         if (ee.type == EAGLE_EVENT_DISPLAY_CLOSE) {
+            quit = true;
+         }
+         if ((ee.type == EAGLE_EVENT_KEY_DOWN) && (ee.keyboard.keycode == EAGLE_KEY_ESCAPE)) {
+            quit = true;
+         }
+      } while (!sys->UpToDate());
+   }
 
 
    return 0;
